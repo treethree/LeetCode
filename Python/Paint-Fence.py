b33bfcbce739295e9241c808ce44fd96ccf233bc
ShiLei Cui <scui5@binghamton.edu>
2018-10-17 12:00:34 -0400

    :+1:

--- Python/Paint-Fence.py
+++ Python/Paint-Fence.py
@@ -12,12 +12,12 @@ class Solution(object):
             return 0
         elif n == 1:
             return k
-        ways = [0] * 3
-        ways[0] = k
-        ways[1] = (k - 1) * ways[0] + k
+        dp = [0] * 3
+        dp[0] = k
+        dp[1] = (k - 1) * dp[0] + k
         for i in xrange(2, n):
-            ways[i % 3] = (k - 1) * (ways[(i - 1) % 3] + ways[(i - 2) % 3])
-        return ways[(n - 1) % 3]
+            dp[i % 3] = (k - 1) * (dp[(i - 1) % 3] + dp[(i - 2) % 3])
+        return dp[(n - 1) % 3]
 
 # Time:  O(n)
 # Space: O(n)
@@ -39,9 +39,9 @@ class Solution2(object):
             return 0
         elif n == 1:
             return k
-        ways = [0] * n
-        ways[0] = k
-        ways[1] = (k - 1) * ways[0] + k
-        for i in xrange(2, n):
-            ways[i] = (k - 1) * (ways[i - 1] + ways[i - 2])
-        return ways[n - 1]
+        dp = [0] * n
+        dp[0] = k
+        dp[1] = (k - 1) * dp[0] + k
+        for i in range(2, n):
+            dp[i] = (k - 1) * (dp[i - 1] + dp[i - 2])
+        return dp[n - 1]
