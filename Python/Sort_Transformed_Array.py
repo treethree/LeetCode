5fffe84ca2127da9cf0ef01e4dbe10a4f6ddf017
ShiLei Cui <scui5@binghamton.edu>
2018-11-10 18:01:12 -0500

    Update Python/Sort_Transformed_Array.py

--- Python/Sort_Transformed_Array.py
+++ Python/Sort_Transformed_Array.py
@@ -42,3 +42,23 @@ class Solution:
         return res
     def getNum(self,x,a,b,c):
         return a * x * x + b * x + c
+
+class Solution2:
+    def sortTransformedArray(self, nums, a, b, c):
+        # Write your code here
+        f = lambda x, a, b, c : a*x*x + b*x + c
+
+        res = []
+        if not nums:
+            return res
+
+        left, right = 0, len(nums) - 1
+        d = -1 if a > 0 else 1
+        while left <= right:
+            if d * f(nums[left], a, b, c) < d * f(nums[right], a, b, c):
+                res.append(f(nums[left], a, b, c))
+                left += 1
+            else:
+                res.append(f(nums[right], a, b, c))
+                right -= 1
+        return res[::d]
